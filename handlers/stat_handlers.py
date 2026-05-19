721975cce5ace0bea9c9b82b7e386574e1afda18
DmitryBashkov <125092029+DmitryBashkov@users.noreply.github.com>
2026-05-19 21:05:06 +0000

    Translate comments and strings to English

--- handlers/stat_handlers.py
+++ handlers/stat_handlers.py
@@ -17,10 +17,10 @@ from config_data.const import STAT_MODES
 import logging
 from os import linesep
 
-# Инициализируем роутер уровня модуля
+# Initialize the module-level router
 router: Router = Router()
 
-# инициализируем логгер
+# Initialize the logger
 logger = logging.getLogger(__name__)
 
 linesep = linesep + '- '
@@ -37,7 +37,7 @@ async def cmd_get_stat(message: Message, bot: Bot, command: CommandObject):
 
     keyboard.adjust(1)
     
-    await message.answer(text = 'Какая статистика тебя интересует?',
+    await message.answer(text = 'Which statistics are you interested in?',
                          reply_markup = keyboard.as_markup())
 
 @router.callback_query(StatCallbackFactory.filter())
@@ -46,35 +46,35 @@ async def get_stat(callback: CallbackQuery, callback_data: StatCallbackFactory, 
     user = User(callback.from_user.username)
     user_stat = callback_data.user
     
-    # смотрим, какую статистику захотел пользователь
+    # Check which statistics the user requested
     match callback_data.mode:
 
-        # дневная статистика
+        # daily statistics
         case 'day_stat':
             file_to_send = get_day_stat(user = user, user_stat = user_stat)
             if file_to_send == False:
                 await bot.send_message(chat_id = user.chat_id,
-                                       text = 'На сегодня данных нет')
+                                       text = 'No data for today')
             else:
                 await bot.send_photo(chat_id = user.chat_id, 
                                     photo = file_to_send)
         
-        # недельная статистика
+        # weekly statistics
         case 'week_stat':
             file_to_send = get_week_stat(user = user, user_stat = user_stat)
             if file_to_send == False:
                 await bot.send_message(chat_id = user.chat_id,
-                                       text = 'На сегодня данных нет')
+                                       text = 'No data for today')
             else:
                 await bot.send_photo(chat_id = user.chat_id, 
                                            photo = file_to_send)
         
-        # статистика по вопросам с самым большим количеством неправильных ответов
+        # statistics for questions with the most incorrect answers
         case 'failed_questions':
             msg_text: str = ''
             question_list = get_failed_questions(user = user, user_stat = user_stat)
             for question in question_list:
-                msg_text += f'<b>{question[0]}</b>\n<i>({question[1]} неправильных)</i>\n\n'
+                msg_text += f'<b>{question[0]}</b>\n<i>({question[1]} incorrect)</i>\n\n'
             await bot.send_message(chat_id = user.chat_id,
                                    text = msg_text,
                                    parse_mode = 'HTML')
