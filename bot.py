76b8de71f86ffd2d9fc9eaf67be9aefe54eef798
DmitryBashkov <125092029+DmitryBashkov@users.noreply.github.com>
2026-05-19 21:04:48 +0000

    Translate comments and strings to English

--- bot.py
+++ bot.py
@@ -16,9 +16,9 @@ from logging import handlers
 
 
 '''
-конфигурируем логирование
-логи будут писаться в файлы, каждый новый день -- отдельный файл
-новый файл с логами будет открываться в полночь
+Configure logging.
+Logs will be written to files, one file per day.
+A new log file will be opened at midnight.
 '''
 
 logger = logging.getLogger()
@@ -33,34 +33,34 @@ logger.info('Proverkin Bot started')
 # logging.basicConfig(level = logging.INFO)
 # logger.info(f'Quick Quiz Bot 0.4 started')
 bot: Bot = None
-# Функция конфигурирования и запуска бота
+# Function to configure and start the bot
 
 async def main():
 
-    # Загружаем конфиг в переменную config
+    # Load config into the config variable
     # config: Config = load_config()
     
-    # Инициализируем бот и диспетчер
+    # Initialize the bot and dispatcher
     bot: Bot = Bot(token = config.tg_bot.token)
     
     # storage: MemoryStorage = MemoryStorage()
     # dp: Dispatcher = Dispatcher(storage = storage)
 
-    # Регистриуем роутеры в диспетчере
+    # Register routers in the dispatcher
     dp.include_router(admin_handlers.router)
     dp.include_router(user_handlers.router)
     dp.include_router(new_user_handlers.router)
     dp.include_router(stat_handlers.router)
     dp.include_router(quiz_handlers.router)
 
-    # Создаем список с командами и их описанием для кнопки menu
+    # Create the list of commands and their descriptions for the menu button
     main_menu_commands = [
         BotCommand(
             command = '/quiz',
-            description = 'Запустить квиз прямо сейчас'),
+            description = 'Start a quiz right now'),
         BotCommand(
             command = '/restart',
-            description = 'Если что-то пошло не так и вы перестали получать запланированные квизы'),
+            description = 'If something went wrong and you stopped receiving scheduled quizzes'),
     ]
 
     # setting commands for the bot
@@ -73,15 +73,15 @@ async def main():
     total_users = sqlite3_connector.get_user_qty()
     total_scheduled_users = 0
 
-    # запускаем проверку на наличие квиза для все пользователей
+    # Schedule quizzes for all active users
     for user in sqlite3_connector.get_all_active_users_with_chat_id():
         await schedule_quiz(bot, user[0], user[1], notify = False)
         total_scheduled_users += 1
 
-    logging.info(f'Запланированы квизы для {total_scheduled_users} из {total_users} пользователей')
+    logging.info(f'Quizzes scheduled for {total_scheduled_users} out of {total_users} users')
 
 
-    # Пропускаем накопившиеся апдейты и запускаем polling
+    # Skip accumulated updates and start polling
     await bot.delete_webhook(drop_pending_updates = True)
     # await dp.startup.register(set_main_menu)
     await dp.start_polling(bot)
